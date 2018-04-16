c0cd19139fd798ccd31821cda8c0d5ad58b94ace
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-04-16 00:14:15 -0700

    Add some simple state handling.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,14 @@ import logo from '../assets/logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ppsspp: null,
+			connecting: false,
+		};
+	}
+
 	render() {
 		return (
 			<div className="App">
@@ -13,27 +21,45 @@ class App extends Component {
 				</header>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
-					<div><button onClick={this.handleConnect}>Connect</button></div>
 				</p>
+				<div>{this.button()}</div>
 			</div>
 		);
 	}
 
+	button() {
+		if (this.state.connecting) {
+			return <button disabled="disabled">Connecting...</button>;
+		} else if (this.state.ppsspp) {
+			return <button onClick={this.handleDisconnect}>Disconnect</button>;
+		}
+		return <button onClick={this.handleConnect}>Connect</button>;
+	}
+
 	handleConnect = () => {
 		let ppsspp = new PPSSPP();
+		this.setState({ connecting: true });
 
 		ppsspp.onError = (message, level) => {
 			console.log(message);
 		};
 		ppsspp.onClose = () => {
 			console.log('Disconnected');
+			this.setState({ ppsspp: null, connecting: false });
 		};
 
 		ppsspp.autoConnect().then(() => {
 			console.log('Connected');
+			this.setState({ ppsspp, connecting: false });
 		}, err => {
 			console.log('Connect failed', err);
+			this.setState({ ppsspp: null, connecting: false });
 		});
+	}
+
+	handleDisconnect = () => {
+		// Should trigger the appropriate events automatically.
+		this.state.ppsspp.disconnect();
 	}
 }
 
