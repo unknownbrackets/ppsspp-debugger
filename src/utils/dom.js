03660a5633d17242a0c1d2b852b059f10fd24007
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-04-26 17:47:27 -0700

    Make ensureInView respect scrollable areas.

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,13 +1,37 @@
+function isSubsetBounds(bounds, parentBounds) {
+	if (bounds.top < parentBounds.top || bounds.left < parentBounds.left) {
+		return false;
+	}
+	if (bounds.bottom > parentBounds.bottom || bounds.right > parentBounds.right) {
+		return false;
+	}
+	return true;
+}
+
 export function ensureInView(node, options) {
+	const windowBounds = { top: 0, left: 0, bottom: window.innerHeight, right: window.innerWidth };
 	const rect = node.getBoundingClientRect();
-	if (rect.top < 0 || rect.left < 0) {
+	if (!isSubsetBounds(rect, windowBounds)) {
 		node.scrollIntoView(options);
 		return true;
 	}
-	if (rect.bottom >= window.innerHeight || rect.right >= window.innerWidth) {
-		node.scrollIntoView(options);
-		return true;
+
+	let parentNode = node;
+	while ((parentNode = parentNode.parentNode) !== null) {
+		if (parentNode instanceof HTMLElement) {
+			const style = window.getComputedStyle(parentNode);
+			const overflows = style.overflow + style.overflowX + style.overflowY;
+			if (style.position === 'static' && overflows.indexOf('auto') === -1 && overflows.indexOf('scroll') === -1) {
+				continue;
+			}
+
+			if (!isSubsetBounds(rect, parentNode.getBoundingClientRect())) {
+				node.scrollIntoView(options);
+				return true;
+			}
+		}
 	}
+
 	return false;
 }
 
