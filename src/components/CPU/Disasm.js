334ad6ef08dbcee30546cbc7e8c062c0679ef3ba
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-04-26 17:48:26 -0700

    Split out disasm list management.

--- src/components/CPU/Disasm.js
+++ src/components/CPU/Disasm.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import DisasmBranchGuide from './DisasmBranchGuide';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import DisasmContextMenu from './DisasmContextMenu';
-import DisasmLine from './DisasmLine';
+import DisasmList from './DisasmList';
 import listeners from '../../utils/listeners.js';
-import { hasContextMenu } from '../../utils/dom';
 import './Disasm.css';
 
 const MIN_BUFFER = 100;
 const MAX_BUFFER = 500;
 
-class Disasm extends Component {
+class Disasm extends PureComponent {
 	constructor(props) {
 		super(props);
 
@@ -20,14 +19,14 @@ class Disasm extends Component {
 			lineHeight: 0,
 			visibleLines: 0,
 			displaySymbols: true,
-			mouseDown: false,
+			wantDisplaySymbols: true,
 			cursor: null,
 		};
 		this.updatesSequence = Promise.resolve(null);
 		this.updatesPending = 0;
 
+		this.ref = React.createRef();
 		this.listRef = React.createRef();
-		this.cursorRef = React.createRef();
 
 		this.needsScroll = false;
 		this.needsOffsetFix = false;
@@ -44,66 +43,66 @@ class Disasm extends Component {
 	}
 
 	render() {
-		const offsets = this.calcOffsets();
 		const events = {
+			onScroll: ev => this.onScroll(ev),
 			onDragStart: ev => ev.preventDefault(),
-			onMouseDownCapture: ev => this.onMouseDown(ev),
-			onMouseUpCapture: this.state.mouseDown ? (ev => this.onMouseUp(ev)) : undefined,
-			onMouseMove: this.state.mouseDown ? (ev => this.onMouseMove(ev)) : undefined,
-			onKeyDown: ev => this.onKeyDown(ev),
 		};
 
 		return [
-			<div key="disasm" className="Disasm" onScroll={ev => this.onScroll(ev)}>
-				<div className="Disasm__list" ref={this.listRef} {...events}>
-					{this.state.lines.map((line) => this.renderLine(line))}
-					{this.state.branchGuides.map((guide) => this.renderBranchGuide(guide, offsets))}
-				</div>
+			<div key="disasm" ref={this.ref} className="Disasm" {...events}>
+				<DisasmList ref={this.listRef} {...this.state}
+					onDoubleClick={this.onDoubleClick}
+					updateSelection={this.props.updateSelection}
+					updateCursor={this.updateCursor}
+					updateDisplaySymbols={this.updateDisplaySymbols}
+					selectionTop={this.props.selectionTop}
+					selectionBottom={this.props.selectionBottom}
+				/>
 			</div>,
 			this.renderContextMenu(),
 		];
-	}
-
-	renderLine(line) {
-		const { displaySymbols } = this.state;
-		const props = {
-			displaySymbols,
-			line,
-			selected: line.address >= this.props.selectionTop && line.address <= this.props.selectionBottom,
-			cursor: line.address === this.state.cursor,
-			onDoubleClick: this.onDoubleClick,
-			ref: line.address === this.state.cursor ? this.cursorRef : undefined,
-		};
-
-		return <DisasmLine key={line.address} contextmenu="disasm" {...props} />;
-	}
-
-	renderBranchGuide(guide, offsets) {
-		const key = String(guide.top) + String(guide.bottom) + guide.direction;
-		const { range, lineHeight } = this.state;
-		const selected = guide.top === this.state.cursor || guide.bottom === this.state.cursor;
-
-		return <DisasmBranchGuide {...{ key, guide, range, offsets, lineHeight, selected }} />;
 	}
 
 	renderContextMenu() {
 		return <DisasmContextMenu key="menu" stepping={this.props.stepping} />;
 	}
 
-	calcOffsets() {
-		// TODO: This is sorta ugly, but...
-		let pos = 0;
-		let offsets = {};
-		this.state.lines.forEach((line) => {
-			offsets[line.address] = pos;
-			pos += this.state.lineHeight;
-		});
-		return offsets;
+	updateCursor = (cursor) => {
+		if (this.state.cursor !== cursor) {
+			this.needsScroll = 'nearest';
+			this.setState({ cursor });
+
+			let { start, end } = this.state.range;
+			const minBuffer = Math.max(MIN_BUFFER, this.state.visibleLines);
+			const maxBuffer = Math.max(MAX_BUFFER, this.state.visibleLines * 5);
+
+			// This is here for keyboard scrolling, mainly.
+			if (cursor - minBuffer * 4 < start) {
+				start = cursor - minBuffer * 2 * 4;
+			} else if (cursor - maxBuffer * 4 > start) {
+				start = cursor - (maxBuffer - minBuffer) * 4;
+			}
+			if (cursor + minBuffer * 4 > end) {
+				end = cursor + minBuffer * 2 * 4;
+			} else if (cursor + maxBuffer * 4 < end) {
+				end = cursor + (maxBuffer - minBuffer) * 4;
+			}
+
+			if (start !== this.state.range.start || end !== this.state.range.end) {
+				if (this.updatesPending === 0) {
+					this.updateDisasm({ start, end });
+				}
+			}
+		}
+	}
+
+	updateDisplaySymbols = (flag) => {
+		this.setState({ wantDisplaySymbols: flag });
 	}
 
 	getSnapshotBeforeUpdate(prevProps, prevState) {
-		if (this.needsOffsetFix && this.cursorRef.current) {
-			const top = this.cursorRef.current.boundingTop();
+		if (this.needsOffsetFix && this.listRef.current) {
+			const top = this.listRef.current.cursorBoundingTop();
 			if (top) {
 				return { top };
 			}
@@ -137,8 +136,8 @@ class Disasm extends Component {
 			if (foundLine) {
 				this.setState({ lineHeight: foundLine.clientHeight });
 			}
-		} else if (this.listRef.current) {
-			const visibleLines = Math.floor(this.listRef.current.parentNode.clientHeight / lineHeight);
+		} else if (this.ref.current) {
+			const visibleLines = Math.floor(this.ref.current.clientHeight / lineHeight);
 			if (visibleLines !== this.state.visibleLines) {
 				this.setState({ visibleLines });
 			}
@@ -148,20 +147,21 @@ class Disasm extends Component {
 			this.needsScroll = 'center';
 		}
 		// Always associated with a state update.
-		if (this.needsScroll && this.cursorRef.current) {
-			this.cursorRef.current.ensureInView(this.needsScroll);
+		if (this.needsScroll && this.listRef.current) {
+			this.listRef.current.ensureCursorInView(this.needsScroll);
 			this.needsScroll = false;
 		}
 
-		if (snapshot && this.cursorRef.current) {
-			const top = this.cursorRef.current.boundingTop();
-			this.listRef.current.parentNode.scrollTop -= snapshot.top - top;
+		if (snapshot && this.listRef.current) {
+			const top = this.listRef.current.cursorBoundingTop();
+			this.ref.current.scrollTop -= snapshot.top - top;
 			this.needsOffsetFix = false;
 		}
 	}
 
 	updateDisasm(newRange) {
 		const minBuffer = Math.max(MIN_BUFFER, this.state.visibleLines);
+		const displaySymbols = this.state.wantDisplaySymbols;
 		const updateRange = {
 			address: newRange === null ? this.props.selectionTop - minBuffer * 4: newRange.start,
 			count: newRange === null ? minBuffer * 2 : undefined,
@@ -173,7 +173,7 @@ class Disasm extends Component {
 			return this.props.ppsspp.send({
 				event: 'memory.disasm',
 				...updateRange,
-				displaySymbols: this.state.displaySymbols,
+				displaySymbols,
 			}).then((data) => {
 				const { range, branchGuides, lines } = data;
 				if (newRange === null) {
@@ -181,7 +181,7 @@ class Disasm extends Component {
 				} else {
 					this.needsOffsetFix = true;
 				}
-				this.setState({ range, branchGuides, lines });
+				this.setState({ range, branchGuides, lines, displaySymbols });
 			}, (err) => {
 				this.setState({ range: { start: 0, end: 0 }, branchGuides: [], lines: [] });
 			}).finally(() => {
@@ -190,79 +190,8 @@ class Disasm extends Component {
 		});
 	}
 
-	handleTodo = (ev, data) => {
-		// TODO
-		console.log(data);
-	}
-
 	onDoubleClick = (ev, data) => {
 		console.log('breakpoint', data);
-	}
-
-	onMouseDown(ev) {
-		const line = this.mouseEventToLine(ev);
-		// Don't change selection if right clicking within the selection.
-		if (ev.button !== 2 || line.address < this.props.selectionTop || line.address > this.props.selectionBottom) {
-			this.applySelection(ev, line);
-		} else if (this.state.cursor !== line.address) {
-			// But do change the cursor.
-			this.setState({ cursor: line.address });
-		}
-		if (ev.button === 0) {
-			this.setState({ mouseDown: true });
-		}
-	}
-
-	onMouseUp(ev) {
-		const line = this.mouseEventToLine(ev);
-		if (line) {
-			this.applySelection(ev, line);
-		}
-		// For focus.
-		this.needsScroll = 'nearest';
-		this.setState({ mouseDown: false });
-	}
-
-	onMouseMove(ev) {
-		if (ev.buttons === 0) {
-			this.needsScroll = 'nearest';
-			this.setState({ mouseDown: false });
-			return;
-		}
-
-		const line = this.mouseEventToLine(ev);
-		if (line) {
-			this.applySelection(ev, line);
-		}
-	}
-
-	onKeyDown(ev) {
-		if (hasContextMenu()) {
-			return;
-		}
-		if (ev.key === 'ArrowUp' || ev.key === 'ArrowDown' || ev.key === 'PageUp' || ev.key === 'PageDown') {
-			const lineIndex = this.findCursorLineIndex();
-
-			let dist = 1;
-			if (ev.key === 'PageUp' || ev.key === 'PageDown') {
-				dist = this.state.visibleLines;
-			}
-			if (ev.key === 'ArrowUp' || ev.key === 'PageUp') {
-				dist = -dist;
-			}
-
-			const newIndex = Math.min(this.state.lines.length - 1, Math.max(0, lineIndex + dist));
-			if (lineIndex !== newIndex) {
-				this.needsScroll = 'nearest';
-				this.applySelection(ev, this.state.lines[newIndex]);
-				ev.preventDefault();
-			}
-		}
-
-		if (ev.key === 'Tab') {
-			this.setState({ displaySymbols: !this.state.displaySymbols });
-			ev.preventDefault();
-		}
 	}
 
 	onScroll(ev) {
@@ -292,76 +221,15 @@ class Disasm extends Component {
 			}
 		}
 	}
-
-	applySelection(ev, line) {
-		let { selectionTop, selectionBottom } = this.props;
-		if (ev.shiftKey) {
-			selectionTop = Math.min(selectionTop, line.address);
-			selectionBottom = Math.max(selectionBottom, line.address);
-		} else {
-			selectionTop = line.address;
-			selectionBottom = line.address;
-		}
-
-		if (selectionTop !== this.props.selectionTop || selectionBottom !== this.props.selectionBottom) {
-			this.props.updateSelection({
-				selectionTop,
-				selectionBottom,
-			});
-		}
-		if (this.state.cursor !== line.address) {
-			this.setState({ cursor: line.address });
-		}
-
-		let { start, end } = this.state.range;
-		const minBuffer = Math.max(MIN_BUFFER, this.state.visibleLines);
-		const maxBuffer = Math.max(MAX_BUFFER, this.state.visibleLines * 5);
-
-		// This is here for keyboard scrolling.
-		if (line.address - minBuffer * 4 < start) {
-			start = line.address - minBuffer * 2 * 4;
-		} else if (line.address - maxBuffer * 4 > start) {
-			start = line.address - (maxBuffer - minBuffer) * 4;
-		}
-		if (line.address + minBuffer * 4 > end) {
-			end = line.address + minBuffer * 2 * 4;
-		} else if (line.address + maxBuffer * 4 < end) {
-			end = line.address + (maxBuffer - minBuffer) * 4;
-		}
-
-		if (start !== this.state.range.start || end !== this.state.range.end) {
-			if (this.updatesPending === 0) {
-				this.updateDisasm({ start, end });
-			}
-		}
-	}
-
-	mouseEventToLine(ev) {
-		// Account for page and list scrolling.
-		const y = ev.pageY - this.listRef.current.getBoundingClientRect().top - window.scrollY;
-		const index = Math.floor(y / this.state.lineHeight);
-		return this.state.lines[index];
-	}
-
-	findCursorLineIndex() {
-		const addr = this.state.cursor ? this.state.cursor : this.props.selectionTop;
-		for (let i = 0; i < this.state.lines.length; ++i) {
-			if (this.state.lines[i].address === addr) {
-				return i;
-			}
-		}
-
-		return undefined;
-	}
 }
 
-Disasm.defaultProps = {
-	ppsspp: null,
-	log: null,
-	selectionTop: null,
-	selectionBottom: null,
-	stepping: false,
-	updateSelection: null,
+Disasm.propTypes = {
+	ppsspp: PropTypes.object.isRequired,
+	log: PropTypes.func.isRequired,
+	updateSelection: PropTypes.func.isRequired,
+	selectionTop: PropTypes.number,
+	selectionBottom: PropTypes.number,
+	stepping: PropTypes.bool.isRequired,
 };
 
 export default Disasm;
---
+++ src/components/CPU/DisasmList.js
@@ -0,0 +1,221 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import DisasmBranchGuide from './DisasmBranchGuide';
+import DisasmLine from './DisasmLine';
+import { hasContextMenu } from '../../utils/dom';
+
+class DisasmList extends PureComponent {
+	state = {
+		mouseDown: false,
+	};
+	ref;
+	cursorRef;
+
+	constructor(props) {
+		super(props);
+
+		this.ref = React.createRef();
+		this.cursorRef = React.createRef();
+	}
+
+	render() {
+		const events = {
+			onMouseDownCapture: ev => this.onMouseDown(ev),
+			onMouseUpCapture: this.state.mouseDown ? (ev => this.onMouseUp(ev)) : undefined,
+			onMouseMove: this.state.mouseDown ? (ev => this.onMouseMove(ev)) : undefined,
+			onKeyDown: ev => this.onKeyDown(ev),
+		};
+
+		const offsets = this.calcOffsets();
+		return (
+			<div className="Disasm__list" ref={this.ref} {...events}>
+				{this.props.lines.map((line) => this.renderLine(line))}
+				{this.props.branchGuides.map((guide) => this.renderBranchGuide(guide, offsets))}
+			</div>
+		);
+	}
+
+	calcOffsets() {
+		// TODO: Cache this and update when lines update.
+		let pos = 0;
+		let offsets = {};
+		this.props.lines.forEach((line) => {
+			offsets[line.address] = pos;
+			pos += this.props.lineHeight;
+		});
+		return offsets;
+	}
+
+	renderLine(line) {
+		const { selectionTop, selectionBottom, displaySymbols, cursor } = this.props;
+		const props = {
+			displaySymbols,
+			line,
+			selected: line.address >= selectionTop && line.address <= selectionBottom,
+			cursor: line.address === cursor,
+			onDoubleClick: this.props.onDoubleClick,
+			ref: line.address === cursor ? this.cursorRef : undefined,
+		};
+
+		return <DisasmLine key={line.address} contextmenu="disasm" {...props} />;
+	}
+
+	renderBranchGuide(guide, offsets) {
+		const key = String(guide.top) + String(guide.bottom) + guide.direction;
+		const { range, lineHeight, cursor } = this.props;
+		const props = {
+			key,
+			guide,
+			offsets,
+			range,
+			lineHeight,
+			selected: guide.top === cursor || guide.bottom === cursor,
+		};
+
+		return <DisasmBranchGuide {...props} />;
+	}
+
+	// Exposed to parent.
+	cursorBoundingTop() {
+		if (this.cursorRef.current) {
+			return this.cursorRef.current.boundingTop();
+		}
+		return null;
+	}
+
+	// Exposed to parent.
+	ensureCursorInView(options) {
+		if (this.cursorRef.current) {
+			this.cursorRef.current.ensureInView(options);
+		}
+	}
+
+	onMouseDown(ev) {
+		const line = this.mouseEventToLine(ev);
+		// Don't change selection if right clicking within the selection.
+		if (ev.button !== 2 || line.address < this.props.selectionTop || line.address > this.props.selectionBottom) {
+			this.applySelection(ev, line);
+		} else {
+			// But do change the cursor.
+			this.props.updateCursor(line.address);
+		}
+		if (ev.button === 0) {
+			this.setState({ mouseDown: true });
+		}
+	}
+
+	onMouseUp(ev) {
+		const line = this.mouseEventToLine(ev);
+		if (line) {
+			this.applySelection(ev, line);
+		}
+		this.setState({ mouseDown: false });
+	}
+
+	onMouseMove(ev) {
+		if (ev.buttons === 0) {
+			this.setState({ mouseDown: false });
+			return;
+		}
+
+		const line = this.mouseEventToLine(ev);
+		if (line) {
+			this.applySelection(ev, line);
+		}
+	}
+
+	onKeyDown(ev) {
+		if (hasContextMenu()) {
+			return;
+		}
+		if (ev.key === 'ArrowUp' || ev.key === 'ArrowDown' || ev.key === 'PageUp' || ev.key === 'PageDown') {
+			const lineIndex = this.findCursorLineIndex();
+
+			let dist = 1;
+			if (ev.key === 'PageUp' || ev.key === 'PageDown') {
+				dist = this.props.visibleLines;
+			}
+			if (ev.key === 'ArrowUp' || ev.key === 'PageUp') {
+				dist = -dist;
+			}
+
+			const newIndex = Math.min(this.props.lines.length - 1, Math.max(0, lineIndex + dist));
+			if (lineIndex !== newIndex) {
+				this.applySelection(ev, this.props.lines[newIndex]);
+				ev.preventDefault();
+			}
+		}
+
+		if (ev.key === 'Tab') {
+			this.props.updateDisplaySymbols(!this.props.displaySymbols);
+			ev.preventDefault();
+		}
+	}
+
+	applySelection(ev, line) {
+		let { selectionTop, selectionBottom } = this.props;
+		if (ev.shiftKey) {
+			selectionTop = Math.min(selectionTop, line.address);
+			selectionBottom = Math.max(selectionBottom, line.address);
+		} else {
+			selectionTop = line.address;
+			selectionBottom = line.address;
+		}
+
+		if (selectionTop !== this.props.selectionTop || selectionBottom !== this.props.selectionBottom) {
+			this.props.updateSelection({
+				selectionTop,
+				selectionBottom,
+			});
+		}
+		this.props.updateCursor(line.address);
+	}
+
+	mouseEventToLine(ev) {
+		// Account for page and list scrolling.
+		const y = ev.pageY - this.ref.current.getBoundingClientRect().top - window.scrollY;
+		const index = Math.floor(y / this.props.lineHeight);
+		return this.props.lines[index];
+	}
+
+	findCursorLineIndex() {
+		const { cursor, lines, selectionTop } = this.props;
+		const addr = cursor ? cursor : selectionTop;
+		for (let i = 0; i < lines.length; ++i) {
+			if (lines[i].address === addr) {
+				return i;
+			}
+		}
+
+		return undefined;
+	}
+}
+
+DisasmList.propTypes = {
+	lineHeight: PropTypes.number.isRequired,
+	visibleLines: PropTypes.number.isRequired,
+	displaySymbols: PropTypes.bool.isRequired,
+	cursor: PropTypes.number,
+	selectionTop: PropTypes.number,
+	selectionBottom: PropTypes.number,
+
+	onDoubleClick: PropTypes.func.isRequired,
+	updateCursor: PropTypes.func.isRequired,
+	updateSelection: PropTypes.func.isRequired,
+	updateDisplaySymbols: PropTypes.func.isRequired,
+
+	range: PropTypes.shape({
+		start: PropTypes.number.isRequired,
+		end: PropTypes.number.isRequired,
+	}).isRequired,
+	lines: PropTypes.arrayOf(PropTypes.shape({
+		address: PropTypes.number.isRequired,
+	})).isRequired,
+	branchGuides: PropTypes.arrayOf(PropTypes.shape({
+		top: PropTypes.number.isRequired,
+		bottom: PropTypes.number.isRequired,
+		direction: PropTypes.string.isRequired,
+	})).isRequired,
+};
+
+export default DisasmList;
