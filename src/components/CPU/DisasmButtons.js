b42088e565c2728bb31bd2d956bf1c8b93b21c11
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-04-29 21:59:41 -0700

    Add stepping actions.

--- src/components/CPU/DisasmButtons.js
+++ src/components/CPU/DisasmButtons.js
@@ -20,11 +20,11 @@ class DisasmButtons extends PureComponent {
 					{this.props.stepping || !this.props.started ? 'Go' : 'Stop'}
 				</button>
 				<span className="DisasmButtons__spacer"></span>
-				<button type="button" disabled={disabled}>Step Into</button>
-				<button type="button" disabled={disabled}>Step Over</button>
-				<button type="button" disabled={disabled}>Step Out</button>
+				<button type="button" disabled={disabled} onClick={this.handleStepInto}>Step Into</button>
+				<button type="button" disabled={disabled} onClick={this.handleStepOver}>Step Over</button>
+				<button type="button" disabled={disabled} onClick={this.handleStepOut}>Step Out</button>
 				<span className="DisasmButtons__spacer"></span>
-				<button type="button" disabled={disabled}>Next HLE</button>
+				<button type="button" disabled={disabled} onClick={this.handleNextHLE}>Next HLE</button>
 				<span className="DisasmButtons__spacer"></span>
 				<button type="button" onClick={this.handleBreakpointOpen} disabled={!this.state.connected}>Breakpoint</button>
 				<span className="DisasmButtons__spacer"></span>
@@ -50,6 +50,38 @@ class DisasmButtons extends PureComponent {
 	handleGoStop = () => {
 		this.props.ppsspp.send({
 			event: this.props.stepping ? 'cpu.resume' : 'cpu.stepping',
+		}).catch(() => {
+			// Already logged, let's assume the parent will have marked it disconnected/not started by now.
+		});
+	}
+
+	handleStepInto = () => {
+		this.props.ppsspp.send({
+			event: 'cpu.stepInto',
+		}).catch(() => {
+			// Already logged, let's assume the parent will have marked it disconnected/not started by now.
+		});
+	}
+
+	handleStepOver = () => {
+		this.props.ppsspp.send({
+			event: 'cpu.stepOver',
+		}).catch(() => {
+			// Already logged, let's assume the parent will have marked it disconnected/not started by now.
+		});
+	}
+
+	handleStepOut = () => {
+		this.props.ppsspp.send({
+			event: 'cpu.stepOut',
+		}).catch(() => {
+			// Already logged, let's assume the parent will have marked it disconnected/not started by now.
+		});
+	}
+
+	handleNextHLE = () => {
+		this.props.ppsspp.send({
+			event: 'cpu.nextHLE',
 		}).catch(() => {
 			// Already logged, let's assume the parent will have marked it disconnected/not started by now.
 		});
