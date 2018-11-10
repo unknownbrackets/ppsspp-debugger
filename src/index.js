224e6fa79493a50cb0e4143df2e1daef448bb596
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-11-10 10:48:06 -0800

    Update dependencies.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'babel-polyfill';
+import 'react-app-polyfill/ie11';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 Modal.setAppElement('#root');
