9121c799194548824e2abdeb4601d232d1e5cda1
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-11-17 23:16:12 -0800

    Include GE Record button and update React.

---
+++ precache-manifest.6e5cf908fc5b7c4c757c7829406fd15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d4a183a91e06076f9aa0198e550d999",
+    "url": "/static/media/logo.1d4a183a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fd98bcbc267a0f6d0de",
+    "url": "/static/js/main.0fd98bcb.chunk.js"
+  },
+  {
+    "revision": "a8395144fa8de8d414b1",
+    "url": "/static/js/1.a8395144.chunk.js"
+  },
+  {
+    "revision": "0fd98bcbc267a0f6d0de",
+    "url": "/static/css/main.76bafe7b.chunk.css"
+  },
+  {
+    "revision": "a8395144fa8de8d414b1",
+    "url": "/static/css/1.77a9bf8e.chunk.css"
+  },
+  {
+    "revision": "876b68f9814afc08f6da41bf7efa18be",
+    "url": "/index.html"
+  }
+];
