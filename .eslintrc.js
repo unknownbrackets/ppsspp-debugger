f60585f6451d1e0db379162891c5b0c413b4f048
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-06-07 21:01:54 -0700

    Update eslint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: 'react-app',
+	extends: ['react-app', 'plugin:jsdoc/recommended'],
 	env: {
 		browser: true,
 		commonjs: true,
@@ -10,16 +10,22 @@ module.exports = {
 		ecmaVersion: 6,
 		sourceType: 'module',
 	},
+	plugins: [
+		'jsdoc',
+	],
 	rules: {
 		'curly': 'warn',
 		'comma-dangle': ['error', 'only-multiline'],
 		'indent': ['error', 'tab'],
 		'no-extra-semi': 'error',
 		'semi': ['error', 'always'],
+		'jsdoc/require-jsdoc': 0,
+		'jsx-quotes': ['error', 'prefer-double'],
 		'no-unexpected-multiline': 'warn',
 		'no-extra-boolean-cast': 'warn',
 		'no-unsafe-finally': 'warn',
 		'no-irregular-whitespace': 'error',
+		'object-curly-spacing': ['error', 'always'],
 		'block-scoped-var': 'error',
 		'consistent-return': 'warn',
 		'yoda': 'error',
